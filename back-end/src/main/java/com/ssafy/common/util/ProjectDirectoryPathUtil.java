ef6c5605367bd8b69c7637cb9c4d16be4eaae984
김현민 <ml2439@naver.com>
2022-02-11 16:22:12 +0900

    22/02/11 image upload path update3

--- back-end/src/main/java/com/ssafy/common/util/ProjectDirectoryPathUtil.java
+++ back-end/src/main/java/com/ssafy/common/util/ProjectDirectoryPathUtil.java
@@ -13,7 +13,7 @@ public class ProjectDirectoryPathUtil {
 
     public static String getProfileImageDirectoryPath() {
         // 현재 프로젝트 경로
-        String projectPath = "https://i6e202.p.ssafy.io:8443";
+        String projectPath = "/home/ubuntu/docker-volume/jenkins/workspace/s06p12e202_deploy/back-end";
         // 폴더 경로
         String directoryPath =
                 "src" + File.separator + // File.seperator는 OS종속적
