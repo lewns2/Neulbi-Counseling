85e9442f902556ef6caa2bbfdb57b60589bb458c
김현민 <ml2439@naver.com>
2022-02-11 17:29:58 +0900

    22/02/11 image upload path update7

--- back-end/src/main/java/com/ssafy/common/util/ProjectDirectoryPathUtil.java
+++ back-end/src/main/java/com/ssafy/common/util/ProjectDirectoryPathUtil.java
@@ -13,14 +13,15 @@ public class ProjectDirectoryPathUtil {
 
     public static String getProfileImageDirectoryPath() {
         // 현재 프로젝트 경로
-        String projectPath =
-                File.separator + "home" +
-                File.separator + "ubuntu" +
-                File.separator + "docker-volume" +
-                File.separator + "jenkins" +
-                File.separator + "workspace" +
-                File.separator + "s06p12e202_deploy" +
-                File.separator + "back-end";
+        String projectPath = System.getProperty("user.dir");
+//                File.separator + "home" +
+//                File.separator + "ubuntu" +
+//                File.separator + "docker-volume" +
+//                File.separator + "jenkins" +
+//                File.separator + "workspace" +
+//                File.separator + "s06p12e202_deploy" +
+//                File.separator + "back-end";
+
 
         // 폴더 경로
         String directoryPath =
@@ -30,7 +31,7 @@ public class ProjectDirectoryPathUtil {
                         "static" + File.separator +
                         "profile_img";
 
-        return projectPath + File.separator + directoryPath;
+        return projectPath + directoryPath;
     }
 
     public static String getProfileImagePath(String fileName) {
