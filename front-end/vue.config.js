c9c8a757ee14183d1a7f082a8475ded640695999
wkdwls1004 <wkdwls1004@naver.com>
2022-02-14 01:22:40 +0900

    설정 변경

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,3 @@
-
 // Vue3 관련 설정 파일
 module.exports = {
   devServer: {
@@ -6,19 +5,17 @@ module.exports = {
     port: 8083,
     open: true,
     proxy: {
-      '/': {
-        target: 'https://i6e202.p.ssafy.io:8443/'
-      }
+      "/": {
+        target: "https://i6e202.p.ssafy.io:8443/",
+      },
     },
     historyApiFallback: true,
-    hot: true
+    hot: true,
   },
   css: {
-    requireModuleExtension: false // import 시에 경로에 .module 포함 안해도 됨.
+    requireModuleExtension: false, // import 시에 경로에 .module 포함 안해도 됨.
   },
-  transpileDependencies: [
-    'element-plus'
-  ],
+  transpileDependencies: ["element-plus"],
   lintOnSave: false,
-  outputDir: '/app/dist'
-}
+  outputDir: "../back-end/src/main/resources/dist",
+};
