74d99ce39526669e9bdb75a042b7304f6c1d4edb
박영찬 <wkdwls1004@naver.com>
2022-02-13 16:47:56 +0900

    director

--- back-end/src/main/java/com/ssafy/common/util/ProjectDirectoryPathUtil.java
+++ back-end/src/main/java/com/ssafy/common/util/ProjectDirectoryPathUtil.java
@@ -31,7 +31,7 @@ public class ProjectDirectoryPathUtil {
                         "static" + File.separator +
                         "profile_img";
 
-        return projectPath + directoryPath;
+        return projectPath + "/" + directoryPath;
     }
 
     public static String getProfileImagePath(String fileName) {
